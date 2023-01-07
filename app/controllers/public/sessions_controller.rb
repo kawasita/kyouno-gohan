313b8127c953ae8fdcc6566754fadadcddead3ee
kawasita <115086200+kawasita@users.noreply.github.com>
2023-01-07 15:06:28 +0000

    退会済みか判断

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,7 +18,15 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  # 退会しているかを判断するメソッド
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    return if !@user
+    if @user.valid_password?(params[:user][:password])
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
