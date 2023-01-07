762a55fef193f22f9acde9e0421b154e04de213f
kawasita <115086200+kawasita@users.noreply.github.com>
2023-01-07 13:31:01 +0000

    deviseルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users
+  devise_for :admins
+  
+  # 利用者
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
