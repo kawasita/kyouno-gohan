ef4303d50ff8b89ce071cfdb368e4e850ec09d05
kawasita <115086200+kawasita@users.noreply.github.com>
2023-01-18 01:48:02 +0000

    登録情報編集画面

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,7 +2,22 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
+  
+  
+  protected
+  
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname, :email])
+  end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(_resource)
+    users_path
+  end
 
   # GET /resource/sign_up
   # def new
