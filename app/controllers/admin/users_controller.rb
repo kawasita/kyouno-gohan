5b57e7435949a1005a4b79b43b1a876caf335993
kawasita <115086200+kawasita@users.noreply.github.com>
2023-01-21 03:09:30 +0000

    adminでuserのステータスチェック

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,11 +4,15 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_user_path(@user)
+      redirect_to admin_user_path(@user), notice: '更新完了しました'
     else
       render :edit
     end
